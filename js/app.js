e28d9ed2e803842613d3b9874f5b9635a3bf0d74
Olawale Malomo <warlay234@gmail.com>
2019-01-21 23:19:08 +0100

    Integrate github

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var followersApp = {
     root: document.querySelector('.followers-app'),
     display: {},
-    api: {},
+    api: {
+        baseUrl: 'https://api.github.com'
+    },
     main: {}
 };
 
@@ -18,6 +20,25 @@ var followersApp = {
 
     var resultsEl = app.root.querySelector('.results');
 
+    var userResultsEl = resultsEl.querySelector('.results__user');
+    var userNoneEl = resultsEl.querySelector('.results__user-none');
+
+    var userProfileEl = userResultsEl.querySelector('.profile');
+    var userImg = userProfileEl.querySelector('.profile__img');
+    var userName = userProfileEl.querySelector('.profile__name');
+    var userRepos = userProfileEl.querySelector('.profile__repos');
+    var userFollowers = userProfileEl.querySelector('.profile__followers');
+    var userGithub = userProfileEl.querySelector('.profile__github-link');
+
+
+    var ffResultsEl = resultsEl.querySelector('.results__ff');
+    var ffNoneEl = resultsEl.querySelector('.results__ff-none');
+    var ffLoader = resultsEl.querySelector('.results__ff-loader');
+    var ffList = ffResultsEl.querySelector('.card__list');
+
+    var mainAppCallBack = function () {
+    };
+
     searchInput.addEventListener('onkeypress', trimInput);
     searchAlertDismiss.addEventListener('click', function (e) {
         e.preventDefault();
@@ -25,12 +46,15 @@ var followersApp = {
     });
 
     function initSearch(callback) {
+        mainAppCallBack = callback;
+
         searchBtn.addEventListener('click', function (e) {
             e.preventDefault();
+            fixSearch();
+
             if (!searchInput.value) {
                 showSearchError('Please enter a username');
                 resultsEl.style.display = 'block';
-                fixSearch();
                 return;
             }
 
@@ -50,7 +74,97 @@ var followersApp = {
     function onSearchStart() {
         searchLoader.style.display = 'block';
         resultsEl.style.display = 'none';
-        fixSearch();
+    }
+
+    function onSearchEnd(user) {
+        searchLoader.style.display = 'none';
+        resultsEl.style.display = 'block';
+
+        if (!user) {
+            userResultsEl.style.display = 'none';
+            userNoneEl.style.display = 'block';
+            return;
+        }
+
+        userNoneEl.style.display = 'none';
+        userResultsEl.style.display = 'block';
+        populateUser(user);
+    }
+
+    function onFollowersLoadStart() {
+        ffLoader.style.display = 'block';
+        ffResultsEl.style.display = 'none';
+        ffNoneEl.style.display = 'none';
+    }
+
+    function onFollowersLoadEnd(followers) {
+        ffLoader.style.display = 'none';
+
+        if (!followers.length) {
+            ffResultsEl.style.display = 'none';
+            ffNoneEl.style.display = 'block';
+            return;
+        }
+
+        ffNoneEl.style.display = 'none';
+        ffResultsEl.style.display = 'block';
+        populateFollowers(followers);
+    }
+
+    function populateUser(user) {
+        userImg.setAttribute('src', user.avatar_url);
+        userName.innerHTML = user.name
+            ? (user.name + ' <span class="profile__username">(' + user.login + ')</span>')
+            : user.login;
+        userRepos.textContent = user.public_repos;
+        userFollowers.textContent = user.followers + ' follower(s) / Following ' + user.following;
+        userGithub.setAttribute('href', user.html_url);
+    }
+
+    function populateFollowers(followers) {
+        ffList.innerHTML = null;
+        followers.forEach(function (follower) {
+            var item = document.createElement('li');
+            item.classList.add('card__item');
+
+            var profile = document.createElement('div');
+            profile.classList.add('profile');
+
+            var avatar = document.createElement('div');
+            avatar.classList.add('profile__avatar');
+            avatar.innerHTML = '<img class="profile__img" src="' + follower.avatar_url + '" alt="profile image">';
+
+            var info = document.createElement('div');
+            info.classList.add('profile__info');
+
+            var name = document.createElement('div');
+            name.classList.add('profile__name');
+            name.textContent = follower.login;
+
+            var githubLink = document.createElement('a');
+            githubLink.classList.add('profile__github-link');
+            githubLink.setAttribute('href', follower.html_url);
+            githubLink.textContent = 'View on github';
+
+            var profileLink = document.createElement('a');
+            profileLink.classList.add('profile__github-link');
+            profileLink.textContent = 'View profile';
+            profileLink.addEventListener('click', function (e) {
+                e.preventDefault();
+                mainAppCallBack(follower.login);
+                onSearchStart();
+            });
+
+            info.appendChild(name);
+            info.appendChild(profileLink);
+            info.appendChild(githubLink);
+
+            profile.appendChild(avatar);
+            profile.appendChild(info);
+
+            item.appendChild(profile);
+            ffList.appendChild(item);
+        });
     }
 
     function showSearchError(message) {
@@ -72,6 +186,9 @@ var followersApp = {
     }
 
     app.display.initSearch = initSearch;
+    app.display.onSearchEnd = onSearchEnd;
+    app.display.onFollowersLoadStart = onFollowersLoadStart;
+    app.display.onFollowersLoadEnd = onFollowersLoadEnd;
     app.display.showSearchError = showSearchError;
     app.display.showSearchSuccess = showSearchSuccess;
 
@@ -79,6 +196,57 @@ var followersApp = {
 
 
 (function api(app) {
+    var lastResponse = {};
+
+    function fetchUser(query, onUserFetch) {
+        var url = app.api.baseUrl + '/users/' + query;
+        fetch(url)
+            .then(function (response) {
+                lastResponse = response;
+                return response.json()
+            })
+            .then(function (json) {
+                if (lastResponse.status === 200) {
+                    onUserFetch(null, json);
+                    return;
+                }
+                onUserFetch(json);
+            }).catch(function (err) {
+            onUserFetch(err);
+        });
+    }
+
+    function fetchFollowers(url, onFollowersFetch) {
+        fetch(url)
+            .then(function (response) {
+                lastResponse = response;
+                return response.json()
+            })
+            .then(function (json) {
+                var nextUrl = getLinks(lastResponse.headers.get('link')).next;
+                if (lastResponse.status === 200) {
+                    onFollowersFetch(null, json, nextUrl);
+                    return;
+                }
+                onFollowersFetch(json);
+            }).catch(function (err) {
+            onFollowersFetch(err);
+        });
+    }
+
+    function getLinks(responseLink) {
+        var links = {};
+        responseLink.split(',').forEach(function (linkComp) {
+            var url = linkComp.substring(linkComp.indexOf('<') + 1, linkComp.indexOf('>'));
+            var rel = linkComp.substring(linkComp.indexOf('rel="') + 4).replace('"', '');
+            links[rel] = url;
+        });
+
+        return links;
+    }
+
+    app.api.fetchUser = fetchUser;
+    app.api.fetchFollowers = fetchFollowers;
 
 })(followersApp);
 
@@ -86,8 +254,29 @@ var followersApp = {
 (function main(app) {
 
     app.display.initSearch(function (query) {
-        console.log(query);
+        app.api.fetchUser(query, onUserFetch);
     });
 
+    function onUserFetch(err, user) {
+        if (err) {
+            app.display.showSearchError(err.message);
+            app.display.onSearchEnd();
+            return;
+        }
+        console.log(err, user);
+
+        app.display.onSearchEnd(user);
+        app.api.fetchFollowers(user.followers_url, onFollowersFetch);
+    }
+
+    function onFollowersFetch(err, followers) {
+        if (err) {
+            app.display.showSearchError(err.message);
+            app.display.onFollowersLoadEnd();
+            return;
+        }
+
+        app.display.onFollowersLoadEnd(followers);
+    }
 
 })(followersApp);
